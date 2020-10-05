6790fdb1af3af76415976e9c536dc368064b0152
digitalgust <digitalgust@163.com>
2020-10-05 13:11:55 +0800

    update test starter script

--- binary/centos_x64/test.sh
+++ binary/centos_x64/test.sh
@@ -1,7 +1,7 @@
 
-./mini_jvm -cp ../lib/minijvm_rt.jar:../libex/glfw_gui.jar test/Boing
-./mini_jvm -cp ../lib/minijvm_rt.jar:../libex/janino.jar:../libex/commons-compiler.jar org.codehaus.janino.Compiler  ../res/BpDeepTest.java
+./mini_jvm -bootclasspath ../lib/minijvm_rt.jar -cp ../libex/glfw_gui.jar test/Boing
+./mini_jvm -bootclasspath ../lib/minijvm_rt.jar -cp ../libex/janino.jar:../libex/commons-compiler.jar org.codehaus.janino.Compiler  ../res/BpDeepTest.java
 
 echo execute BpDeepTest
-./mini_jvm -cp ../lib/minijvm_rt.jar:../res/ BpDeepTest
-./mini_jvm -cp ../lib/minijvm_rt.jar:../libex/luaj.jar Sample
+./mini_jvm -bootclasspath ../lib/minijvm_rt.jar -cp ../res/ BpDeepTest
+./mini_jvm -bootclasspath ../lib/minijvm_rt.jar -cp ../libex/luaj.jar Sample
